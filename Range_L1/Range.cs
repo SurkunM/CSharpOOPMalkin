e33b9b1c5fe6527a26b3bff6a095a63a7bedb4f4
surkun1992@gmail.com <surkun1992@gmail.com>
2024-07-13 21:21:08 +0700

    Добавьте файлы проекта.

---
+++ Range_L1/Range.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range_L1;
+
+internal class Range
+{
+    public Range? FirstRange { get; set; }
+
+    public double FirstFrom { get; set; }
+
+    public double FirstTo { get; set; }
+
+    public Range? SecondRange { get; set; }
+
+    public double SecondFrom { get; set; }
+
+    public double SecondTo { get; set; }
+
+    public Range(double from, double to)
+    {
+        FirstFrom = from;
+        FirstTo = to;
+    }
+
+    public Range(Range firstRange, Range secondRange)
+    {
+        FirstRange = firstRange;
+
+        FirstFrom = FirstRange.FirstFrom;
+        FirstTo = FirstRange.FirstTo;
+
+        SecondRange = secondRange;
+
+        SecondFrom = SecondRange.FirstFrom;
+        SecondTo = SecondRange.FirstTo;
+    }
+
+    public Range GetIntersectionRange()
+    {
+        if (FirstTo > SecondFrom && SecondTo > FirstFrom)
+        {
+            if (FirstFrom < SecondFrom)
+            {
+                if (FirstTo < SecondTo)
+                {
+                    return new Range(SecondFrom, FirstTo);
+                }
+                else
+                {
+                    return new Range(SecondFrom, SecondTo);
+                }
+            }
+            else
+            {
+                if (FirstTo < SecondTo)
+                {
+                    return new Range(FirstFrom, FirstTo);
+                }
+                else
+                {
+                    return new Range(FirstFrom, SecondTo);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public Range[] CombiningRange()
+    {
+        if (FirstTo > SecondFrom && SecondTo > FirstFrom)
+        {
+            if (FirstFrom < SecondFrom)
+            {
+                if (FirstTo > SecondTo)
+                {
+                    return [new Range(FirstFrom, FirstTo)];
+                }
+                else
+                {
+                    return [new Range(FirstFrom, SecondTo)];
+                }
+            }
+            else
+            {
+                if (FirstTo > SecondTo)
+                {
+                    return [new Range(SecondFrom, FirstTo)];
+                }
+                else
+                {
+                    return [new Range(SecondFrom, SecondTo)];
+                }
+            }
+        }
+
+        return [FirstRange, SecondRange];
+    }
+
+    public Range[] SubtractionRange()
+    {
+        if (FirstTo > SecondFrom && SecondTo > FirstFrom)
+        {
+            if (FirstFrom < SecondFrom)
+            {
+                if (FirstTo > SecondTo)
+                {
+                    return [new Range(FirstFrom, SecondFrom), new Range(SecondTo, FirstTo)];
+                }
+                else
+                {
+                    return [new Range(FirstFrom, SecondFrom)];
+                }
+            }
+            else
+            {
+                if (FirstTo > SecondTo)
+                {
+                    return [new Range(SecondTo, FirstTo)];
+                }
+                else
+                {
+                    return [];
+                }
+            }
+        }
+
+        return [FirstRange];
+    }
+
+    public double GetLength()
+    {
+        return FirstTo - FirstFrom;
+    }
+
+    public bool IsInside(double number)
+    {
+        return number > FirstFrom && number < FirstTo ? true : false;
+    }
+
+    public void PrintRangeLength()
+    {
+        Console.WriteLine("Range length = {0}", GetLength());
+
+    }
+
+    public void PrintRangeInterval()
+    {
+        Console.WriteLine("{0} - {1}", FirstFrom, FirstTo);
+    }
+}
---
+++ Range_L1/RangeMain.cs
@@ -0,0 +1,43 @@
+﻿namespace Range_L1;
+
+internal class RangeMain
+{
+    static void Main(string[] args)
+    {
+        double firstFrom = 0;
+        double firstTo = 10;
+
+        double secondFrom = 10;
+        double secondTo = 25;
+
+        Range firstRange = new Range(firstFrom, firstTo);
+
+        double number = 5;
+
+        if (firstRange.IsInside(number))
+        {
+            Console.WriteLine("Число принадлежит диапазону длинна которой {0}", firstRange.GetLength());
+        }
+        else
+        {
+            Console.WriteLine("Число не принадлежит диапазону длинна которой {0}", firstRange.GetLength());
+
+        }
+
+        Range secondRange = new Range(secondFrom, secondTo);
+
+        Range rangeInterval = new Range(firstRange, secondRange).GetIntersectionRange();
+
+        if (rangeInterval == null)
+        {
+            Console.WriteLine("Интервалы не пересекаются");
+        }
+        else
+        {
+            rangeInterval.PrintRangeInterval();
+        }
+
+        Range[] rangeCombining = new Range(firstRange, secondRange).CombiningRange();
+        Range[] rangeSubtraction = new Range(firstRange, secondRange).SubtractionRange();
+    }
+}
