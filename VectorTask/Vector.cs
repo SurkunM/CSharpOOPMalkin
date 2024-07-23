e0e12cf594b3f3a1d003e8b5012c1018775ef921
surkun1992@gmail.com <surkun1992@gmail.com>
2024-07-23 20:47:34 +0700

    Вектор

---
+++ VectorTask/Vector.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorTask;
+
+internal class Vector
+{
+    private const int pointsCount = 2;
+
+    public double[] VectorComponents {  get; set; }
+
+    private int dimension  = 1;
+
+    public int Dimension
+    {
+        get
+        {
+            return dimension;
+        }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                dimension = value;
+            }
+        }
+    }
+
+    public Vector(int dimension)
+    {
+        Dimension = dimension;   
+        
+        VectorComponents = new double[dimension * pointsCount];
+    }
+
+    public Vector(Vector vector)
+    {
+        VectorComponents = vector.VectorComponents;
+    }
+
+    public Vector(double[] components)
+    {
+        VectorComponents = components;
+    }
+
+    public Vector(int dimension, double[] components)
+    {
+        Dimension = dimension;
+        VectorComponents = GetComponentsCopy(components);
+    }
+
+    private double[] GetComponentsCopy(double[] components)
+    {
+        double[] array = new double[Dimension];
+
+        if (Dimension > components.Length)
+        {
+            for (int i = 0; i < components.Length; i++)
+            {
+                array[i] = components[i];
+            }
+        }
+        else
+        {
+            array = components;
+        }
+
+        return array;
+    }
+
+    public int GetSize()
+    {
+        return VectorComponents.Length;
+    }
+
+    public override string? ToString()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        string semicolon = "; ";
+
+        foreach (double components in VectorComponents)
+        {
+            stringBuilder.Append(components).Append(semicolon);
+        }
+
+        stringBuilder.Remove(stringBuilder.Length - semicolon.Length, semicolon.Length);
+
+        return $"{{{stringBuilder.ToString()}}}";
+    }
+}
---
+++ VectorTask/VectorMine.cs
@@ -0,0 +1,18 @@
+﻿namespace VectorTask
+{
+    internal class VectorMine
+    {
+        static void Main(string[] args)
+        {
+            double[] array = { 0, 3, 1.1, 4.4, 0.4 };
+
+            Vector vector4 = new Vector(8, array);
+
+           string s = vector4.ToString();
+
+
+            Console.WriteLine(s);
+
+        }
+    }
+}
