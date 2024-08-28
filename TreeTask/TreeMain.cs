7fe42b821515c85a6b557a305832c940c9a7c9b4
surkun1992@gmail.com <surkun1992@gmail.com>
2024-08-28 15:58:09 +0700

    Tree 1.0

---
+++ TreeTask/TreeMain.cs
@@ -0,0 +1,27 @@
+﻿namespace TreeTask;
+
+internal class TreeMain
+{
+    static void Main(string[] args)
+    {
+        BinaryTree<int> binaryTree = new BinaryTree<int>();
+
+        binaryTree.Add(10);
+        binaryTree.Add(11);
+        binaryTree.Add(1);
+        binaryTree.Add(5);
+        binaryTree.Add(0);
+        binaryTree.Add(4);
+        binaryTree.Add(6);
+        binaryTree.Add(7);
+        binaryTree.Add(15);
+        binaryTree.Add(12);
+        binaryTree.Add(18);
+        binaryTree.Add(4);
+        binaryTree.Add(16);
+
+        binaryTree.BreadthFirstSearch();
+        binaryTree.Visit(binaryTree.GetRoot());
+        binaryTree.DepthFirstSearch();
+    }
+}
---
+++ TreeTask/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿namespace TreeTask;
+
+internal class TreeNode<T> : IComparable<T>
+{
+    public TreeNode<T>? Left { get; set; }
+
+    public TreeNode<T>? Right { get; set; }
+
+    public T Data { get; }
+
+    public TreeNode(T value)
+    {
+        Data = value;
+    }
+
+    public int CompareTo(T? data)
+    {
+        return ((IComparable<T>)Data!).CompareTo(data);
+    }
+}
