24360f52e7e70f8ea5beb8562a0c41d15ed05b15
surkun1992@gmail.com <surkun1992@gmail.com>
2024-11-05 19:56:53 +0700

    Minesweeper 0.001

---
+++ MinesweeperTask/Minesweeper.Logic/Presenter/GamePresenter.cs
@@ -0,0 +1,64 @@
+﻿using Minesweeper.Logic.Model;
+using Minesweeper.Logic.Model.Interface;
+
+namespace Minesweeper.Logic.Presenter;
+
+public class GamePresenter
+{
+    private readonly GameLogic _gameLogic;
+
+    private IGame _viewGame;
+
+    public GamePresenter(GameLogic gameLogic, IGame logicGame)
+    {
+        _gameLogic = gameLogic;
+        _viewGame = logicGame;
+    }
+
+    public void SetFlag(Button button, int row, int column)
+    {
+        if (button.Text == "F")
+        {
+            return;
+        }
+
+        _gameLogic.IsMinedCell(row, column);
+
+        _viewGame.SetUnVisibleCell();
+    }
+
+    public bool HasCellFlag(int row, int column)
+    {
+        return _gameLogic.HasCellFlag(row, column);
+    }
+
+    public void OpenCell(int row, int column)
+    {
+        if (_gameLogic.IsMinedCell(row, column))
+        {
+            _viewGame.OpenMines(row, column);
+            _viewGame.ShowEndGameDialog();
+
+        }
+
+        _viewGame.SetUnVisibleCell();
+    }
+
+    public void ChangeCellFlag(Button button, int row, int column)
+    {
+        if (button.Text == "")
+        {
+            _gameLogic.SetFlag(row, column);
+
+            _viewGame.SetCellFlag();
+        }
+        else
+        {
+            _gameLogic.RemoveFlag(row, column);
+
+            _viewGame.RemoveCellFlag();
+        }
+
+        _viewGame.SetMinesCount(_gameLogic.CurrentMineCount);
+    }
+}
